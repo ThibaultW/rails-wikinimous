c0ba2be577106ff071410c85a6c6ed977dc21d59
Thibault Willems <thibwillems@scarlet.be>
2016-11-22 17:59:21 +0100

    init wikinimous app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do 
+  title = Faker::ChuckNorris.fact
+  content = Lorem::Base.new('paragraphs', 1).output
+  post = Article.new(title: title, content: content)
+  post.save
+end
